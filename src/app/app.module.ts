dbbaf1d43d222ebe78dc824b2271826d9d963fd6
Matthew Davis <matthew@matthewdavis.io>
2018-10-26 15:37:34 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {MatButtonModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
+
   declarations: [
+
     AppComponent
+
   ],
+
   imports: [
+
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+
+    MatButtonModule,
+
   ],
+
   providers: [],
   bootstrap: [AppComponent]
+
 })
-export class AppModule { }
+export class AppModule {
+}
